bad5bf71987ae0919995a75898469b4821f18017
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-08-21 10:24:02 +0600

    blog updated

--- src/components/Pages/home/Blogs.jsx
+++ src/components/Pages/home/Blogs.jsx
@@ -1,27 +1,54 @@
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
 import { FaHeart } from 'react-icons/fa';
 import { BsBoxArrowInDown } from 'react-icons/bs';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Blogs = () => {
-    const responsive = {
-        superLargeDesktop: {
-            // the naming can be any, depends on you.
-            breakpoint: { max: 4000, min: 3000 },
-            items: 5
-        },
-        desktop: {
-            breakpoint: { max: 3000, min: 1024 },
-            items: 3
-        },
-        tablet: {
-            breakpoint: { max: 1024, min: 464 },
-            items: 2
-        },
-        mobile: {
-            breakpoint: { max: 464, min: 0 },
-            items: 1
-        }
+    function Arrow(props) {
+        const { className, style, onClick } = props;
+        return (
+            <div
+                className={className}
+                style={{ ...style, display: "block", background: "#001356", borderRadius: "100%" }}
+                onClick={onClick}
+            />
+        );
+    }
+    const settings = {
+        infinite: false,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 4,
+        nextArrow: <Arrow />,
+        prevArrow: <Arrow />,
+        initialSlide: 0,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     };
     return (
         <div className="container mx-auto mt-24">
@@ -38,8 +65,34 @@ const Blogs = () => {
 
             {/* MAIN CONTENT */}
             <div className="mt-8">
-                <Carousel responsive={responsive}>
-                    <div className="mx-10">
+            <Slider {...settings}>
+                    
+                        <div className="shadow-2xl">
+                            <img className="md:w-full" src="https://i.ibb.co/7X2Hgyg/image-46.png" alt="" />
+                            <div className="">
+                                <h4 className="text-[10px] md:text-base font-medium px-1">John Lewis to make final journey across Edmund Pettus Bridge in procession</h4>
+                                <p className="text-[8px] md:text-sm px-2">The body of the late US Rep. John Lewis on Sunday will make the final journey across the famous bridge in Selma, Alabama, where he helped lead a march for voting rights in 1965.</p>
+                                <p className="mt-4 text-[6px] md:text-sm mb-4"><span className="font-bold">2 hours ago</span> By Lucy Hiddleston  |  4min read</p>
+                                <hr />
+                                <div className="mt-2">
+                                    <div className="flex items-center gap-4">
+                                        <div className="flex items-center gap-2">
+                                            <FaHeart className="text-red-600" />
+                                            <small>39</small>
+                                        </div>
+                                        <div className="flex items-center gap-2">
+                                            <BsBoxArrowInDown />
+                                            <small>72</small>
+                                        </div>
+                                        <div>
+                                            <img src="https://i.ibb.co/vkNfGWz/Vector.png" alt="" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    
+                    
                         <div className="shadow-2xl">
                             <img className="md:w-full" src="https://i.ibb.co/7X2Hgyg/image-46.png" alt="" />
                             <div className="px-2">
@@ -64,8 +117,8 @@ const Blogs = () => {
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="mx-10">
+                    
+                    
                         <div className="shadow-2xl">
                             <img className="md:w-full" src="https://i.ibb.co/7X2Hgyg/image-46.png" alt="" />
                             <div className="px-2">
@@ -90,8 +143,8 @@ const Blogs = () => {
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="mx-10">
+                    
+                    
                         <div className="shadow-2xl">
                             <img className="md:w-full" src="https://i.ibb.co/7X2Hgyg/image-46.png" alt="" />
                             <div className="px-2">
@@ -116,8 +169,8 @@ const Blogs = () => {
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="mx-10">
+                    
+                    
                         <div className="shadow-2xl">
                             <img className="md:w-full" src="https://i.ibb.co/7X2Hgyg/image-46.png" alt="" />
                             <div className="px-2">
@@ -142,8 +195,8 @@ const Blogs = () => {
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="mx-10">
+                    
+                    
                         <div className="shadow-2xl">
                             <img className="md:w-full" src="https://i.ibb.co/7X2Hgyg/image-46.png" alt="" />
                             <div className="px-2">
@@ -168,8 +221,34 @@ const Blogs = () => {
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </Carousel>;
+                    
+                    
+                        <div className="shadow-2xl">
+                            <img className="md:w-full" src="https://i.ibb.co/7X2Hgyg/image-46.png" alt="" />
+                            <div className="px-2">
+                                <h4 className="text-[10px] md:text-base font-medium px-1">John Lewis to make final journey across Edmund Pettus Bridge in procession</h4>
+                                <p className="text-[8px] md:text-sm px-2">The body of the late US Rep. John Lewis on Sunday will make the final journey across the famous bridge in Selma, Alabama, where he helped lead a march for voting rights in 1965.</p>
+                                <p className="mt-4 text-[6px] md:text-sm mb-4"><span className="font-bold">2 hours ago</span> By Lucy Hiddleston  |  4min read</p>
+                                <hr />
+                                <div className="mt-2">
+                                    <div className="flex items-center gap-4">
+                                        <div className="flex items-center gap-2">
+                                            <FaHeart className="text-red-600" />
+                                            <small>39</small>
+                                        </div>
+                                        <div className="flex items-center gap-2">
+                                            <BsBoxArrowInDown />
+                                            <small>72</small>
+                                        </div>
+                                        <div>
+                                            <img src="https://i.ibb.co/vkNfGWz/Vector.png" alt="" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    
+                </Slider>;
             </div>
         </div>
     );
