840b6c2c060f50f8ff6c4e951bcb6fbf7fb51787
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-08-29 11:28:12 +0600

    contact details

--- src/components/Pages/contacts/Contacts.jsx
+++ src/components/Pages/contacts/Contacts.jsx
@@ -11,7 +11,7 @@ const Contacts = () => {
 
     return (
         <div className="container mx-auto mt-5">
-            <div className='grid grid-cols-1 md:grid-cols-2'>
+            <div className='grid grid-cols-1 md:grid-cols-2 bg-white p-2 shadow-2xl rounded-lg'>
                 {/* Contact Info */}
                 <div className='bg-[#001356] text-white pt-8'>
                     <div className='px-8'>
@@ -57,84 +57,122 @@ const Contacts = () => {
                 {/* Contact Form */}
                 <div className='py-8 px-6'>
                     <form onSubmit={handleSubmit(onSubmit)}>
-                        <div>
-                            <label htmlFor="firstName">First Name</label>
-                            <input
-                                type="text"
-                                id="firstName"
-                                {...register('firstName', { required: true })}
+                        <div className='grid grid-cols-2 gap-12'>
+                            <div>
+                                <label className='font-medium text-[#001356]' htmlFor="firstName">First Name</label>
+                                <div>
+                                    <input
+                                        className='outline-none border-b border-gray-500 w-full'
+                                        type="text"
+                                        id="firstName"
+                                        {...register('firstName', { required: true })}
+                                    />
+                                    {formState.errors.firstName && (
+                                        <p className="error">First name is required</p>
+                                    )}
+                                </div>
+                            </div>
+
+                            <div>
+                                <label className='font-medium text-[#001356]' htmlFor="lastName">Last Name</label>
+                                <div>
+                                    <input
+                                        className='outline-none border-b border-gray-500 w-full'
+                                        type="text"
+                                        id="lastName"
+                                        {...register('lastName', { required: true })}
+                                    />
+                                    {formState.errors.lastName && (
+                                        <p className="error">Last name is required</p>
+                                    )}
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='grid grid-cols-2 gap-12 mt-16'>
+                            <div>
+                                <label className='font-medium text-[#001356]' htmlFor="email">Email</label>
+                                <div>
+                                    <input
+                                        className='outline-none border-b border-gray-500 w-full'
+                                        type="email"
+                                        id="email"
+                                        {...register('email', { required: true })}
+                                    />
+                                    {formState.errors.email && <p className="error">Email is required</p>}
+                                </div>
+                            </div>
+
+                            <div>
+                                <label className='font-medium text-[#001356]' htmlFor="phone">Phone Number</label>
+                                <div>
+                                    <input
+                                        className='outline-none border-b border-gray-500 w-full'
+                                        type="tel"
+                                        id="phone"
+                                        {...register('phone', { required: true })}
+                                    />
+                                    {formState.errors.phone && (
+                                        <p className="error">Phone number is required</p>
+                                    )}
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='mt-8'>
+                            <p className='font-bold text-base text-[#001356]'>Selected Subject ?</p>
+                            <div className='flex gap-8 mt-5'>
+                                <label>
+                                    <input
+                                        type="radio"
+                                        value="General Inquiry"
+                                        {...register('inquiryType', { required: true })}
+                                    />
+                                    General Inquiry
+                                </label>
+                                <label>
+                                    <input
+                                        type="radio"
+                                        value="Technical Support"
+                                        {...register('inquiryType', { required: true })}
+                                    />
+                                    General Inquiry
+                                </label>
+                                <label>
+                                    <input
+                                        type="radio"
+                                        value="Feedback"
+                                        {...register('inquiryType', { required: true })}
+                                    />
+                                    General Inquiry
+                                </label>
+                                <label>
+                                    <input
+                                        type="radio"
+                                        value="Feedback"
+                                        {...register('inquiryType', { required: true })}
+                                    />
+                                    General Inquiry
+                                </label>
+                                {formState.errors.inquiryType && (
+                                    <p className="error">Inquiry type is required</p>
+                                )}
+                            </div>
+                        </div>
+
+                        <div className="mb-4 mt-8">
+                            <label className='font-semibold text-gray-400 ml-2' htmlFor="message">Message</label>
+                            <textarea
+                                id="message"
+                                {...register('message', { required: true })}
+                                className="block w-full border-b-2 border-gray-300 outline-none p-2"
+                                placeholder='write your message here'
                             />
-                            {formState.errors.firstName && (
-                                <p className="error">First name is required</p>
-                            )}
                         </div>
 
-                        <div>
-                            <label htmlFor="lastName">Last Name</label>
-                            <input
-                                type="text"
-                                id="lastName"
-                                {...register('lastName', { required: true })}
-                            />
-                            {formState.errors.lastName && (
-                                <p className="error">Last name is required</p>
-                            )}
+                        <div className='flex justify-end mt-5'>
+                            <button className='bg-[#001356] px-8 py-4 rounded-lg text-white' type="submit">Send Message</button>
                         </div>
-
-                        <div>
-                            <label htmlFor="email">Email</label>
-                            <input
-                                type="email"
-                                id="email"
-                                {...register('email', { required: true })}
-                            />
-                            {formState.errors.email && <p className="error">Email is required</p>}
-                        </div>
-
-                        <div>
-                            <label htmlFor="phone">Phone Number</label>
-                            <input
-                                type="tel"
-                                id="phone"
-                                {...register('phone', { required: true })}
-                            />
-                            {formState.errors.phone && (
-                                <p className="error">Phone number is required</p>
-                            )}
-                        </div>
-
-                        <div>
-                            <p>Select Inquiry Type:</p>
-                            <label>
-                                <input
-                                    type="radio"
-                                    value="General Inquiry"
-                                    {...register('inquiryType', { required: true })}
-                                />
-                                General Inquiry
-                            </label>
-                            <label>
-                                <input
-                                    type="radio"
-                                    value="Technical Support"
-                                    {...register('inquiryType', { required: true })}
-                                />
-                                Technical Support
-                            </label>
-                            <label>
-                                <input
-                                    type="radio"
-                                    value="Feedback"
-                                    {...register('inquiryType', { required: true })}
-                                />
-                                Feedback
-                            </label>
-                            {formState.errors.inquiryType && (
-                                <p className="error">Inquiry type is required</p>
-                            )}
-                        </div>
-
-                        <button type="submit">Submit</button>
                     </form>
                 </div>
             </div>
