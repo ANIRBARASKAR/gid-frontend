20711b0e5893169ced7139786204b72e84c91c44
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-08-30 16:36:12 +0600

    icons added

--- src/components/signup-signin/LogIn.jsx
+++ src/components/signup-signin/LogIn.jsx
@@ -1,4 +1,5 @@
 import { useForm } from 'react-hook-form';
+import { MdOutlineMailOutline , MdLockOutline } from 'react-icons/md';
 
 const LogIn = () => {
     const {
@@ -24,26 +25,38 @@ const LogIn = () => {
                     <form onSubmit={handleSubmit(onSubmit)} className="p-4">
                         <div className="mb-4">
                             <label className='text-[#555555]' htmlFor="email">Email :</label>
-                            <input
-                                type="email"
-                                id="email"
-                                {...register('email', { required: true })}
-                                className="block w-full border rounded p-2 bg-[#EEEEEE] outline-none mt-2"
-                            />
+                            <div className='flex items-center'>
+                                <input
+                                    type="email"
+                                    id="email"
+                                    {...register('email', { required: true })}
+                                    className="block w-full border rounded p-2 bg-[#EEEEEE] outline-none mt-2"
+                                />
+                                <div className='bg-[#284B80] text-white pt-3 pb-2 mt-2 px-2 rounded-md'>
+                                    <MdOutlineMailOutline size={20} />
+                                </div>
+                            </div>
                             {errors.email && <p className="text-red-500">Email is required</p>}
                         </div>
+
                         <div className="mb-4">
                             <label className='text-[#555555]' htmlFor="password">Password :</label>
-                            <input
-                                type="password"
-                                id="password"
-                                {...register('password', { required: true })}
-                                className="block w-full border rounded p-2 bg-[#EEEEEE] outline-none mt-2"
-                            />
+                            <div className='flex items-center'>
+                                <input
+                                    type="password"
+                                    id="password"
+                                    {...register('password', { required: true })}
+                                    className="block w-full border rounded p-2 bg-[#EEEEEE] outline-none mt-2"
+                                />
+                                <div className='bg-[#284B80] text-white pt-3 pb-2 mt-2 px-2 rounded-md'>
+                                    <MdLockOutline size={20} />
+                                </div>
+                            </div>
                             {errors.password && <p className="text-red-500">Password is required</p>}
                         </div>
+                        <p className='flex justify-end'><small className='text-[#284B80] border-b border-[#284B80] font-semibold '>Forgot password?</small></p>
                         <button type="submit" className="bg-[#284B80] text-white font-semibold shadow-xl px-12 py-2 rounded mt-10 w-full">
-                        Login now
+                            Login now
                         </button>
                     </form>
                 </div>
