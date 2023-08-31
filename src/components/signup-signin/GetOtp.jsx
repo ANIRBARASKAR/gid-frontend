d0e9c418c87df060e1aa926f7be986b7b6cf9d7f
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-08-31 16:30:45 +0600

    get otp

--- src/components/signup-signin/GetOtp.jsx
+++ src/components/signup-signin/GetOtp.jsx
@@ -12,9 +12,28 @@ const GetOtp = () => {
                         <p className="text-center text-[#555555] font-semibold mt-5">Sign up into your account</p>
                     </div>
                     <form className="p-4">
-                        <button type="submit" className="bg-[#284B80] text-white font-semibold shadow-xl px-12 py-2 rounded mt-10 w-full">
-                            Login now
-                        </button>
+                        <div>
+                            <label htmlFor="username">username :</label>
+                            <input className="w-full border rounded p-2 bg-[#EEEEEE] outline-none mt-2" type="text" name="" id="" placeholder="Enter Email Id / mobile no" />
+                        </div>
+                        <div className="flex justify-center">
+                            <button className="bg-[#284B80] text-white font-medium px-2 py-1 rounded-md mt-8 text-sm">Get OTP</button>
+                        </div>
+                        <div className="flex justify-center mt-10">
+                            <div className="flex items-center gap-4">
+                                <input className="w-10 border border-gray-400 shadow rounded-lg p-2 [#EEEEEE] outline-none mt-2" type="text" name="" id="" />
+                                <input className="w-10 border border-gray-400 shadow rounded-lg p-2 [#EEEEEE] outline-none mt-2" type="text" name="" id="" />
+                                <input className="w-10 border border-gray-400 shadow rounded-lg p-2 [#EEEEEE] outline-none mt-2" type="text" name="" id="" />
+                                <input className="w-10 border border-gray-400 shadow rounded-lg p-2 [#EEEEEE] outline-none mt-2" type="text" name="" id="" />
+                                <input className="w-10 border border-gray-400 shadow rounded-lg p-2 [#EEEEEE] outline-none mt-2" type="text" name="" id="" />
+                                <input className="w-10 border border-gray-400 shadow rounded-lg p-2 [#EEEEEE] outline-none mt-2" type="text" name="" id="" />
+                            </div>
+                        </div>
+                        <div className="flex justify-center">
+                            <button type="submit" className="bg-[#284B80] text-white font-semibold shadow-xl px-12 py-2 rounded mt-10">
+                                Next
+                            </button>
+                        </div>
                     </form>
                 </div>
             </div>
