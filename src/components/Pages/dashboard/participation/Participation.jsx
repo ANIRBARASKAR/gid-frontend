f0391f56e77bc002d6a4f54ca9556f533335fe98
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-09-04 17:32:29 +0600

    event

--- src/components/Pages/dashboard/participation/Participation.jsx
+++ src/components/Pages/dashboard/participation/Participation.jsx
@@ -2,7 +2,10 @@
 const Participation = () => {
     return (
         <div className="mt-5 lg:ml-[300px]">
-            <h1 className="text-[#001356] text-[26px] font-semibold">My Profile</h1>
+            <h1 className="text-[#001356] text-[26px] font-semibold">Participation</h1>
+            <div>
+
+            </div>
         </div>
     );
 };
