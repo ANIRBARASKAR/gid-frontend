279a3a42cd47d485d22491419e2d7c80ca47927b
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-09-05 17:37:39 +0600

    participation responsive

--- src/components/Pages/dashboard/participation/Participation.jsx
+++ src/components/Pages/dashboard/participation/Participation.jsx
@@ -11,16 +11,18 @@ const data = [
 const Participation = () => {
     return (
         <div className="mt-5 lg:ml-[300px]">
-            <h1 className="text-[#001356] text-[26px] font-semibold">Participation</h1>
-            <div className='flex items-center gap-8 mt-5'>
-                <p className="text-[18px] text-[#343A40]">All (50)</p>
-                <div className='flex items-center px-2 py-1 border border-gray-300 bg-[#F6F8FA] rounded-xl'>
-                    <BiSearch className='text-[#343A40]' size={20} />
-                    <input className='outline-none bg-[#F6F8FA] w-[300px]' type="search" name="" id="" placeholder='Search..' />
+            <div className='mx-4 md:mx-0'>
+                <h1 className="text-[#001356] text-[26px] font-semibold">Participation</h1>
+                <div className='flex items-center gap-8 mt-5'>
+                    <p className="text-[18px] text-[#343A40]">All (50)</p>
+                    <div className='flex items-center px-2 py-1 border border-gray-300 bg-[#F6F8FA] rounded-xl'>
+                        <BiSearch className='text-[#343A40]' size={20} />
+                        <input className='outline-none bg-[#F6F8FA] md:w-[300px]' type="search" name="" id="" placeholder='Search..' />
+                    </div>
                 </div>
             </div>
-            <div className='mt-5'>
-                <table className="w-[100%] mt-8">
+            <div className='mt-5 mx-2 md:mx-0'>
+                <table className="w-[100%] mt-8 text-xs md:text-base">
                     <thead>
                         <tr className='text-[#001356]'>
                             <th className='py-4'>No</th>
