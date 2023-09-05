9302cff30eb9ceaa364a28bef81444c3c2c41b5b
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-09-05 17:30:55 +0600

    my ticket responsive

--- src/components/Pages/dashboard/tickets/MyTickets.jsx
+++ src/components/Pages/dashboard/tickets/MyTickets.jsx
@@ -36,18 +36,18 @@ const MyTickets = () => {
         <div className="lg:ml-[300px]">
             <div className="lg:mt-16">
                 <div className="mt-5 flex items-center gap-2">
-                    <h1 className="text-[#001356] text-[26px] font-semibold">My Tickets</h1>
+                    <h1 className="text-[#001356] text-[26px] font-semibold mx-6 lg:mx-0">My Tickets</h1>
                     <img className="w-[35px] h-[35px]" src="https://i.ibb.co/vmB6SLV/clipboard-tick-1.png" alt="" />
                 </div>
                 <div>
                     {
                         tickets.map(item => (
                             <>
-                                <div className="mt-8 bg-white rounded-lg mb-5" style={{
+                                <div className="mt-8 bg-white rounded-lg mb-5 mx-4 md:mx-0" style={{
                                     boxShadow: "0px 4px 10px 0px #0000001A"
                                 }}>
                                     <div className="md:flex items-center justify-between gap-4">
-                                        <div className="p-4 rounded-xl flex gap-8">
+                                        <div className="p-4 rounded-xl flex gap-4 md:gap-8">
                                             <div>
                                                 <img className=" " src={item.img} alt="" />
                                                 <div className="flex justify-end">
@@ -55,8 +55,8 @@ const MyTickets = () => {
                                                 </div>
                                             </div>
                                             <div>
-                                                <h4 className="text-[#284B80] text-xl font-medium">{item.title}</h4>
-                                                <div className="text-[#666666] text-base mt-1">
+                                                <h4 className="text-[#284B80] text-sm md:text-xl font-medium">{item.title}</h4>
+                                                <div className="text-[#666666] text-sm md:text-base mt-1">
                                                     <p>{item.des}</p>
                                                     <p>{item.date}</p>
                                                 </div>
@@ -64,7 +64,7 @@ const MyTickets = () => {
                                         </div>
                                         <div className="px-4 md:px-0">
                                             <p className="text-xl font-medium text-[#001356]">1 ticket</p>
-                                            <div className="textsm text-[#001356]">
+                                            <div className="text-sm text-[#001356]">
                                                 <div className="flex items-center gap-12">
                                                     <p>reference :</p>
                                                     <p>{item.ref}</p>
