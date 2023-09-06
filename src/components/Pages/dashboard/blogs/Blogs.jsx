a1f9a85f9e6af63fd2d496538f59ba9e9bc2bfde
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-09-06 14:36:06 +0600

    blog header added

--- src/components/Pages/dashboard/blogs/Blogs.jsx
+++ src/components/Pages/dashboard/blogs/Blogs.jsx
@@ -2,7 +2,13 @@
 const Blogs = () => {
     return (
         <div className="lg:ml-[300px]">
-            <h1>This is Blog pages</h1>
+            <div className="flex items-center gap-2 mt-8">
+                <h1 className="text-[#001356] text-[26px] font-semibold">Blogs</h1>
+                <img src="https://i.ibb.co/svqRn1R/edit.png" alt="" />
+            </div>
+            <div>
+                
+            </div>
         </div>
     );
 };
