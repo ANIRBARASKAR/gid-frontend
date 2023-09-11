7cc721752af27c4a696ec542293d03b15a62ae29
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-09-11 16:42:11 +0600

    rounds added

--- src/components/Pages/organizerDashboard/eventDetails/buttonPages/rounds/Roundes.jsx
+++ src/components/Pages/organizerDashboard/eventDetails/buttonPages/rounds/Roundes.jsx
@@ -1,7 +1,11 @@
+import { BsArrowLeftCircle, BsArrowRightCircle } from 'react-icons/bs';
+
 
 const Roundes = () => {
     return (
-        <div className="bg-white mt-5 py-8 px-6">
+        <div className="bg-white mt-5 py-8 px-6 rounded-lg" style={{
+            boxShadow: "0px 4px 4px 0px #00000040"
+        }}>
             <p className="text-[12px] md:text-base text-[#001356] font-medium">Add/Manage Rounds</p>
             <div className="border-2 border-[#284B80] rounded-lg pl-4 py-1 flex justify-between">
                 <p className="text-[#284B80]">List all the rounds separately to give users a clear understanding of how they will proceed forward in this opportunity.</p>
@@ -29,6 +33,20 @@ const Roundes = () => {
                         <button className="bg-white text-[#001356] px-6 rounded-md">Quiz</button>
                     </div>
                     <p className="text-[#001356] mt-12">created on 16 Jun, 2023</p>
+                </div>
+            </div>
+            <div className='flex justify-between mt-5'>
+                <div className='flex items-center bg-[#284B80] text-white px-8 py-2 rounded-md gap-4' style={{
+                    boxShadow: "0px 4px 4px 0px #00135640"
+                }}>
+                    <BsArrowLeftCircle size={20} />
+                    <button>Previous</button>
+                </div>
+                <div className='flex items-center bg-[#284B80] text-white px-4 py-2 rounded-md gap-4' style={{
+                    boxShadow: "0px 4px 4px 0px #00135640"
+                }}>
+                    <button>Save & Next</button>
+                    <BsArrowRightCircle size={20} />
                 </div>
             </div>
         </div>
