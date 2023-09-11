0ea3db42403cae30c12a7bed796a8b311aec1096
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-09-11 19:37:12 +0600

    responsive added

--- src/components/Pages/organizerDashboard/eventDetails/EventOrgDetails.jsx
+++ src/components/Pages/organizerDashboard/eventDetails/EventOrgDetails.jsx
@@ -9,41 +9,41 @@ const EventOrgDetails = () => {
     return (
         <div className=" mt-8 lg:ml-[300px]">
             <div>
-                <div className="w-[665px] h-[15px] bg-[#FCFCFC] mx-auto" style={{
+                <div className="w-[275px] md:w-[665px] h-[10px] md:h-[15px] bg-[#FCFCFC] mx-auto" style={{
                     boxShadow: "0px 2px 4px 0px #00000040 inset"
                 }}></div>
-                <div className="flex justify-center mt-[-25px]">
+                <div className="flex justify-center mt-[-15px] md:mt-[-25px]">
                     <div>
-                        <div className="flex justify-between w-[670px]">
-                            <div className="w-[35px] h-[35px] rounded-full bg-[#FCFCFC]" style={{
+                        <div className="flex justify-between w-[280px] md:w-[670px]">
+                            <div className="w-[25px] h-[25px] md:w-[35px] md:h-[35px] rounded-full bg-[#FCFCFC]" style={{
                                 boxShadow: "0px 2px 4px 0px #00000040 inset"
                             }}>
                                 <div>
-                                    <div className="flex justify-center items-center pt-[7px]">
-                                        <div className="w-[22px] h-[22px] rounded-full bg-[#284B80]">
-                                            <p className="text-white text-center">1</p>
+                                    <div className="flex justify-center items-center md:pt-[7px]">
+                                        <div className="w-[18px] h-[18px] md:w-[22px] md:h-[22px] rounded-full bg-[#284B80] mt-1">
+                                            <p className="text-white text-center text-xs md:text-sm mb-1">1</p>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <div className="w-[35px] h-[35px] rounded-full bg-[#FCFCFC]" style={{
+                            <div className="w-[25px] h-[25px] md:w-[35px] md:h-[35px] rounded-full bg-[#FCFCFC]" style={{
                                 boxShadow: "0px 2px 4px 0px #00000040 inset"
                             }}>
                                 <div>
-                                    <div className="flex justify-center items-center pt-[7px]">
-                                        <div className="w-[22px] h-[22px] rounded-full bg-[#284B80]">
-                                            <p className="text-white text-center">2</p>
+                                    <div className="flex justify-center items-center md:pt-[7px]">
+                                        <div className="w-[18px] h-[18px] md:w-[22px] md:h-[22px] rounded-full bg-[#284B80] mt-1">
+                                            <p className="text-white text-center text-xs md:text-sm mb-1">2</p>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <div className="w-[35px] h-[35px] rounded-full bg-[#FCFCFC]" style={{
+                            <div className="w-[25px] h-[25px] md:w-[35px] md:h-[35px] rounded-full bg-[#FCFCFC]" style={{
                                 boxShadow: "0px 2px 4px 0px #00000040 inset"
                             }}>
                                 <div>
-                                    <div className="flex justify-center items-center pt-[7px]">
-                                        <div className="w-[22px] h-[22px] rounded-full bg-[#284B80]">
-                                            <p className="text-white text-center">3</p>
+                                    <div className="flex justify-center items-center md:pt-[7px]">
+                                        <div className="w-[18px] h-[18px] md:w-[22px] md:h-[22px] rounded-full bg-[#284B80] mt-1">
+                                            <p className="text-white text-center text-xs md:text-sm mb-1">3</p>
                                         </div>
                                     </div>
                                 </div>
@@ -51,12 +51,12 @@ const EventOrgDetails = () => {
                         </div>
                     </div>
                 </div>
-                <div className="flex justify-between lg:mx-[280px] mt-1">
+                <div className="flex justify-between mx-[15px] md:mx-[280px] mt-1">
                     <div>
                         <div className="flex justify-center"><RxTriangleUp className="text-[#284B80]" size={25} /></div>
                         <button 
                         onClick={() => setActive("firstButton")}
-                        className="text-white bg-[#284B80] px-2 py-1 rounded-full ml-5 mt-0" style={{
+                        className="text-white bg-[#284B80] text-[8px] md:text-sm px-2 py-1 rounded-full ml-5 mt-0" style={{
                             boxShadow: "0px 2px 4px 0px #00000040 inset"
                         }}>Opportunity Visibility</button>
                     </div>
@@ -64,7 +64,7 @@ const EventOrgDetails = () => {
                         <div className="flex justify-center"><RxTriangleUp className="text-[#D9D9D9]" size={25} /></div>
                         <button 
                         onClick={() => setActive("secondButton")}
-                        className="bg-[#D9D9D9] px-2 py-1 rounded-full" style={{
+                        className="bg-[#D9D9D9] px-2 py-1 rounded-full text-[8px] md:text-sm" style={{
                             boxShadow: "0px 2px 4px 0px #00000040 inset"
                         }}>Rounds</button>
                     </div>
@@ -72,7 +72,7 @@ const EventOrgDetails = () => {
                         <div className="flex justify-center"><RxTriangleUp className="text-[#D9D9D9]" size={25} /></div>
                         <button
                         onClick={() => setActive("thirdButton")} 
-                        className="bg-[#D9D9D9] px-2 py-1 rounded-full" style={{
+                        className="bg-[#D9D9D9] px-2 py-1 rounded-full text-[8px] md:text-sm" style={{
                             boxShadow: "0px 2px 4px 0px #00000040 inset"
                         }}>Rewards & Recognition</button>
                     </div>
--- src/components/Pages/organizerDashboard/eventDetails/buttonPages/rounds/Roundes.jsx
+++ src/components/Pages/organizerDashboard/eventDetails/buttonPages/rounds/Roundes.jsx
@@ -7,32 +7,34 @@ const Roundes = () => {
             boxShadow: "0px 4px 4px 0px #00000040"
         }}>
             <p className="text-[12px] md:text-base text-[#001356] font-medium">Add/Manage Rounds</p>
-            <div className="border-2 border-[#284B80] rounded-lg pl-4 py-1 flex justify-between">
+            <div className="border-2 border-[#284B80] rounded-lg pl-4 py-1 flex justify-between gap-2">
                 <p className="text-[#284B80] text-[10px] md:text-base">List all the rounds separately to give users a clear understanding of how they will proceed forward in this opportunity.</p>
-                <button className="bg-[#284B80] text-white px-2 py-1 rounded-md text-right">Add Round</button>
+                <div className='bg-[#284B80] rounded-md'>
+                    <button className=" text-white px-2 py-1 text-[8px] md:text-base text-right">Add Round</button>
+                </div>
             </div>
             <p className="text-[12px] md:text-base text-[#001356] font-medium mt-5">Round List</p>
             <div className="grid grid-cols-2 md:grid-cols-3 gap-4 md:gap-6 lg:gap-10 mt-5">
-                <div className="bg-[#CFE5FF] h-[138px] p-4 rounded-md">
+                <div className="bg-[#CFE5FF] md:h-[138px] p-4 rounded-md">
                     <div className="flex justify-between">
-                        <h4 className="text-xl font-bold text-[#001356]">Round 1</h4>
-                        <button className="bg-white text-[#001356] px-6 rounded-md">Quiz</button>
+                        <h4 className="text-sm md:text-xl font-bold text-[#001356]">Round 1</h4>
+                        <button className="bg-white text-[#001356] px-6 text-[8px] md:text-base rounded-md">Quiz</button>
                     </div>
-                    <p className="text-[#001356] mt-12">created on 16 Jun, 2023</p>
+                    <p className="text-[#001356] text-xs md:text-base mt-6 md:mt-12">created on 16 Jun, 2023</p>
                 </div>
-                <div className="bg-[#CFE5FF] h-[138px] p-4 rounded-md">
+                <div className="bg-[#CFE5FF] md:h-[138px] p-4 rounded-md">
                     <div className="flex justify-between">
-                        <h4 className="text-xl font-bold text-[#001356]">Round 2</h4>
-                        <button className="bg-white text-[#001356] px-6 rounded-md">Quiz</button>
+                        <h4 className="text-sm md:text-xl font-bold text-[#001356]">Round 2</h4>
+                        <button className="bg-white text-[#001356] px-6 text-[8px] md:text-base rounded-md">Quiz</button>
                     </div>
-                    <p className="text-[#001356] mt-12">created on 16 Jun, 2023</p>
+                    <p className="text-[#001356] text-xs md:text-base mt-6 md:mt-12">created on 16 Jun, 2023</p>
                 </div>
-                <div className="bg-[#CFE5FF] h-[138px] p-4 rounded-md">
+                <div className="bg-[#CFE5FF] md:h-[138px] p-4 rounded-md">
                     <div className="flex justify-between">
-                        <h4 className="text-xl font-bold text-[#001356]">Round 3</h4>
-                        <button className="bg-white text-[#001356] px-6 rounded-md">Quiz</button>
+                        <h4 className="text-sm md:text-xl font-bold text-[#001356]">Round 3</h4>
+                        <button className="bg-white text-[#001356] px-6 text-[8px] md:text-base rounded-md">Quiz</button>
                     </div>
-                    <p className="text-[#001356] mt-12">created on 16 Jun, 2023</p>
+                    <p className="text-[#001356] text-xs md:text-base mt-6 md:mt-12">created on 16 Jun, 2023</p>
                 </div>
             </div>
             <div className='flex justify-between mt-5'>
