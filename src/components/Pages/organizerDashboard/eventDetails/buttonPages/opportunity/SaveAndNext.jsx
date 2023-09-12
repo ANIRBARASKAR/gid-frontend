7fdcac8dad3dad69d0f888a2a788b6bba01e3817
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-09-12 12:56:14 +0600

    next page added

--- src/components/Pages/organizerDashboard/eventDetails/buttonPages/opportunity/SaveAndNext.jsx
+++ src/components/Pages/organizerDashboard/eventDetails/buttonPages/opportunity/SaveAndNext.jsx
@@ -203,11 +203,13 @@ const SaveAndNext = () => {
                     <div className='grid grid-cols-1 md:grid-cols-3 md:gap-8 text-[#001356]'>
                         <div className="mb-4 p-2">
                             <label className="block text-base font-medium">Mode of Payment</label>
-                            <div className='grid grid-cols-2 w-full border border-[#001356] rounded-lg p-1 mt-1'>
-                                <label className="mr-4 flex items-center gap-2">
-                                    <input type="radio" name="gender" value="male" />
-                                    Free Event
-                                </label>
+                            <div className='grid grid-cols-2 gap-4 w-full border border-[#001356] rounded-lg p-1 mt-1'>
+                                <div>
+                                    <label className="mr-4 flex items-center gap-2">
+                                        <input type="radio" name="gender" value="male" />
+                                        Free Event
+                                    </label>
+                                </div>
                                 <div>
                                     <label className="mr-4 flex items-center gap-2">
                                         <input type="radio" name="gender" value="female" />
@@ -227,7 +229,7 @@ const SaveAndNext = () => {
                         </div>
                         <div className="mb-4">
                             <label className="block text-base font-medium">Mode of Event</label>
-                            <div className='grid grid-cols-2 w-full border border-[#001356] rounded-lg p-1 mt-1'>
+                            <div className='grid grid-cols-2 gap-8 w-full border border-[#001356] rounded-lg p-1 mt-1'>
                                 <div>
                                     <label className="mr-4 flex items-center gap-2">
                                         <input type="radio" name="gender" value="male" />
@@ -236,66 +238,190 @@ const SaveAndNext = () => {
                                     <div>
                                         <input
                                             type="text"
-                                            id="charge"
-                                            name="charge"
-                                            className="w-full border-b border-[#001356] p-1 my-4 placeholder-[#001356]"
-                                            placeholder='Enter the charge'
+                                            id="country"
+                                            name="country"
+                                            className="w-full border-b border-[#001356] p-1 mt-2 placeholder-[#001356]"
+                                            placeholder='Country*'
                                         />
                                     </div>
                                     <div>
                                         <input
                                             type="text"
-                                            id="charge"
-                                            name="charge"
-                                            className="w-full border-b border-[#001356] p-1 my-4 placeholder-[#001356]"
-                                            placeholder='Enter the charge'
+                                            id="city"
+                                            name="city"
+                                            className="w-full border-b border-[#001356] p-1 my-2 placeholder-[#001356]"
+                                            placeholder='City*'
                                         />
                                     </div>
                                 </div>
 
 
-                                <label className="mr-4 flex items-center gap-2">
-                                    <input type="radio" name="gender" value="male" />
-                                    Offline Event
-                                </label>
+                                <div>
+                                    <label className="mr-4 flex items-center gap-2">
+                                        <input type="radio" name="gender" value="male" />
+                                        Offline Event
+                                    </label>
+                                    <div>
+                                        <input
+                                            type="text"
+                                            id="state"
+                                            name="state"
+                                            className="w-full border-b border-[#001356] p-1 mt-2 placeholder-[#001356]"
+                                            placeholder='State*'
+                                        />
+                                    </div>
+                                    <div>
+                                        <input
+                                            type="text"
+                                            id="location"
+                                            name="location"
+                                            className="w-full border-b border-[#001356] p-1 my-2 placeholder-[#001356]"
+                                            placeholder='Location*'
+                                        />
+                                    </div>
+                                </div>
+
+
+
                             </div>
                         </div>
                         <div className="mb-4">
                             <label className="block text-base font-medium">Participation</label>
-                            <div className='grid grid-cols-3 w-full border border-[#001356] rounded-lg p-1 mt-1'>
-                                <label className="mr-4 flex items-center gap-2">
-                                    <input type="radio" name="gender" value="male" />
-                                    Individual
-                                </label>
-                                <label className="mr-4 flex items-center gap-2">
-                                    <input type="radio" name="gender" value="female" />
-                                    Team
-                                </label>
+                            <div className='grid grid-cols-3 gap-8 w-full border border-[#001356] rounded-lg p-1 mt-1'>
+                                <div>
+                                    <label className="mr-4 flex items-center gap-2">
+                                        <input type="radio" name="gender" value="male" />
+                                        Individual
+                                    </label>
+                                    <div>
+                                        <input
+                                            type="text"
+                                            id="location"
+                                            name="location"
+                                            className="w-full border-b border-[#001356] p-1 my-2 placeholder-[#001356]"
+                                            placeholder='Location*'
+                                        />
+                                    </div>
+                                </div>
+                                <div>
+                                    <label className="mr-4 flex items-center gap-2">
+                                        <input type="radio" name="gender" value="female" />
+                                        Team
+                                    </label>
+                                    <div>
+                                        <input
+                                            type="text"
+                                            id="location"
+                                            name="location"
+                                            className="w-full border-b border-[#001356] p-1 my-2 placeholder-[#001356]"
+                                            placeholder='Location*'
+                                        />
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div className="mb-4 text-[#001356]">
                         <label className="block text-[#001356] text-base font-medium">Opportunity open for*</label>
-                        <div className="mt-1">
-                            <div className='flex items-center gap-4 '>
-                                <label className="mr-4 flex items-center gap-2">
-                                    <input type="checkbox" name="domain1" />
-                                    School student
-                                </label>
-                                <label className="mr-4 flex items-center gap-2">
-                                    <input type="checkbox" name="domain2" />
-                                    College student
-                                </label>
+                        <div className='grid grid-cols-1 lg:grid-cols-3 gap-4'>
+                            <div className='col-span-1'>
+                                <div className="mt-1">
+                                    <div className='flex items-center gap-4 '>
+                                        <label className="mr-4 flex items-center gap-2">
+                                            <input type="checkbox" name="domain1" />
+                                            School student
+                                        </label>
+                                        <label className="mr-4 flex items-center gap-2">
+                                            <input type="checkbox" name="domain2" />
+                                            College student
+                                        </label>
+                                    </div>
+                                    <div className='flex items-center gap-4 mt-2'>
+                                        <label className="mr-4 flex items-center gap-2">
+                                            <input type="checkbox" name="domain3" />
+                                            Working professionals
+                                        </label>
+                                        <label className="mr-4 flex items-center gap-2">
+                                            <input type="checkbox" name="domain4" />
+                                            Open for All
+                                        </label>
+                                    </div>
+                                </div>
                             </div>
-                            <div className='flex items-center gap-4 mt-2'>
-                                <label className="mr-4 flex items-center gap-2">
-                                    <input type="checkbox" name="domain3" />
-                                    Working professionals
-                                </label>
-                                <label className="mr-4 flex items-center gap-2">
-                                    <input type="checkbox" name="domain4" />
-                                    Open for All
-                                </label>
+                            <div className='col-span-2'>
+                                {/* Selected Students Start */}
+                                <div className='mt-4 border border-[#001356] rounded-md cursor-pointer'>
+                                    <div className='px-2 cursor-pointer'>
+                                        <p className='text-[#001356] text-lg'>Select school student</p>
+                                        <div className='grid grid-cols-7 gap-4 mt-1 pb-2 text-sm'>
+                                            <p className='px-4 py-1 text-center bg-[#E7E7E7] text-gray-500 rounded-md'>1st</p>
+                                            <p className='px-4 py-1 text-center bg-[#E7E7E7] text-gray-500 rounded-md'>2nd</p>
+                                            <p className='px-4 py-1 text-center bg-[#E7E7E7] text-gray-500 rounded-md'>3rd</p>
+                                            <p className='px-4 py-1 text-center bg-[#E7E7E7] text-gray-500 rounded-md'>4th</p>
+                                            <p className='px-4 py-1 text-center bg-[#E7E7E7] text-gray-500 rounded-md'>5th</p>
+                                            <p className='px-4 py-1 text-center bg-[#E7E7E7] text-gray-500 rounded-md'>6th</p>
+                                            <p className='px-4 py-1 text-center bg-[#E7E7E7] text-gray-500 rounded-md'>7th</p>
+                                            <p className='px-4 py-1 text-center bg-[#E7E7E7] text-gray-500 rounded-md'>8th</p>
+                                            <p className='px-4 py-1 text-center bg-[#E7E7E7] text-gray-500 rounded-md'>9th</p>
+                                            <p className='px-4 py-1 text-center bg-[#E7E7E7] text-gray-500 rounded-md'>10th</p>
+                                            <p className='px-4 py-1 text-center bg-[#E7E7E7] text-gray-500 rounded-md'>11th com.</p>
+                                            <p className='px-4 py-1 text-center bg-[#E7E7E7] text-gray-500 rounded-md'>12th com.</p>
+                                            <p className='px-4 py-1 text-center bg-[#E7E7E7] text-gray-500 rounded-md'>11th sci</p>
+                                            <p className='px-4 py-1 text-center bg-[#E7E7E7] text-gray-500 rounded-md'>12th sci</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                {/* Selected Students End */}
+                                {/* Selected college Start */}
+                                <div className='mt-4 border border-[#001356] rounded-md cursor-pointer'>
+                                    <div className='px-2'>
+                                        <p className='text-[#001356] text-lg'>Select college student</p>
+                                        <div className='grid grid-cols-5 gap-4 mt-1 pb-2 text-sm'>
+                                            <p className='px-4 py-1 text-center bg-[#E7E7E7] text-gray-500 rounded-md'>Management</p>
+                                            <p className='px-4 py-1 text-center bg-[#E7E7E7] text-gray-500 rounded-md'>Science & technology</p>
+                                            <p className='px-4 py-1 text-center bg-[#E7E7E7] text-gray-500 rounded-md'>Diploma</p>
+                                            <p className='px-4 py-1 text-center bg-[#E7E7E7] text-gray-500 rounded-md'>Computer Application</p>
+                                            <p className='px-4 py-1 text-center bg-[#E7E7E7] text-gray-500 rounded-md'>Science & Arts</p>
+                                        </div>
+                                        <div className='flex items-center gap-4 mt-2'>
+                                            <p className='px-4 text-center bg-[#E7E7E7] text-gray-500 rounded-md'>Computer</p>
+                                            <p className='px-4 text-center bg-[#E7E7E7] text-gray-500 rounded-md'>Electrical</p>
+                                            <p className='px-4 text-center bg-[#E7E7E7] text-gray-500 rounded-md'>Civil</p>
+                                            <p className='px-4 text-center bg-[#E7E7E7] text-gray-500 rounded-md'>Mechanical</p>
+                                        </div>
+                                        <div className='flex items-center gap-4 my-3'>
+                                            <p className='px-4 text-center bg-[#E7E7E7] text-gray-500 rounded-md'>2023</p>
+                                            <p className='px-4 text-center bg-[#E7E7E7] text-gray-500 rounded-md'>2024</p>
+                                            <p className='px-4 text-center bg-[#E7E7E7] text-gray-500 rounded-md'>2025</p>
+                                            <p className='px-4 text-center bg-[#E7E7E7] text-gray-500 rounded-md'>2026</p>
+                                            <p className='px-4 text-center bg-[#E7E7E7] text-gray-500 rounded-md'>2027</p>
+                                            <p className='px-4 text-center bg-[#E7E7E7] text-gray-500 rounded-md'>2028</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                {/* Selected college End */}
+                                {/* Select working professionals experience Start */}
+                                <div className='mt-4 border border-[#001356] rounded-md cursor-pointer'>
+                                    <div className='px-2'>
+                                        <p className='text-[#001356] text-lg'>Select working professionals experience</p>
+                                        <div className='grid grid-cols-7 gap-4 mt-1 pb-2 text-sm'>
+                                            <p className='px-4 py-1 text-center bg-[#E7E7E7] text-gray-500 rounded-md'>All</p>
+                                            <p className='px-4 py-1 text-center bg-[#E7E7E7] text-gray-500 rounded-md'>Upto 11 months</p>
+                                            <p className='px-4 py-1 text-center bg-[#E7E7E7] text-gray-500 rounded-md'>1 year</p>
+                                            <p className='px-4 py-1 text-center bg-[#E7E7E7] text-gray-500 rounded-md'>2year</p>
+                                            <p className='px-4 py-1 text-center bg-[#E7E7E7] text-gray-500 rounded-md'>3year</p>
+                                            <p className='px-4 py-1 text-center bg-[#E7E7E7] text-gray-500 rounded-md'>4year</p>
+                                            <p className='px-4 py-1 text-center bg-[#E7E7E7] text-gray-500 rounded-md'>5year</p>
+                                            <p className='px-4 py-1 text-center bg-[#E7E7E7] text-gray-500 rounded-md'>6year</p>
+                                            <p className='px-4 py-1 text-center bg-[#E7E7E7] text-gray-500 rounded-md'>7year</p>
+                                            <p className='px-4 py-1 text-center bg-[#E7E7E7] text-gray-500 rounded-md'>8year</p>
+                                            <p className='px-4 py-1 text-center bg-[#E7E7E7] text-gray-500 rounded-md'>9year</p>
+                                            <p className='px-4 py-1 text-center bg-[#E7E7E7] text-gray-500 rounded-md'>10year</p>
+                                            <p className='px-4 py-1 text-center bg-[#E7E7E7] text-gray-500 rounded-md'>10years</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                {/* Select working professionals experience END */}
                             </div>
                         </div>
                     </div>
