d36847aa7f41c644c27af727b31f162eddfabfe3
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-09-16 11:58:54 +0600

    amd progressbar

---
+++ src/components/Pages/adminDashboard/admDashboardHome/AdmProgress.jsx
@@ -0,0 +1,29 @@
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+
+const AdmProgress = () => {
+    const percentage = 70
+    return (
+        <div className="flex items-center gap-4">
+            <div className="progress-container" style={{ width: 40, height: 40 }}>
+                <div className="progress">
+                    <CircularProgressbar
+                        className="custom-progress"
+                        value={70}
+                        strokeWidth={10}
+                        styles={buildStyles({
+                            pathColor: '#001356',
+                            textColor: '#000',
+                            trailColor: '#ADADAD',
+                        })}
+                    />
+                </div>
+            </div>
+            <div className="percentage-label">
+                <p className="text-lg font-semibold text-[#001356]">{percentage}%</p>
+            </div>
+        </div>
+    );
+};
+
+export default AdmProgress;
---
+++ src/components/Pages/adminDashboard/admDashboardHome/AmdLineProgress.jsx
@@ -0,0 +1,11 @@
+
+
+const AmdLineProgress = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AmdLineProgress;
