90bf53de631d496d72bc270d1be26bd25441a362
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-09-16 12:00:37 +0600

    adm line progressbar

--- src/components/Pages/adminDashboard/admDashboardHome/AmdLineProgress.jsx
+++ src/components/Pages/adminDashboard/admDashboardHome/AmdLineProgress.jsx
@@ -1,9 +1,15 @@
-
+import ProgressBar from "@ramonak/react-progress-bar";
 
 const AmdLineProgress = () => {
+    const completed = 80;
+
     return (
         <div>
-            
+            <div className="flex justify-between  text-[#001356] font-medium">
+                {completed}%
+                <p className="text-xs">105 participation</p>
+            </div>
+            <ProgressBar className="w-[100%]" completed={80} />
         </div>
     );
 };
