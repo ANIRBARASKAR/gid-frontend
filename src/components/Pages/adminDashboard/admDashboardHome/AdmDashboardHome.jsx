687a554e4cbf47bc5d5ed10e2583c2c726f7d053
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-09-16 12:58:02 +0600

    order summery

--- src/components/Pages/adminDashboard/admDashboardHome/AdmDashboardHome.jsx
+++ src/components/Pages/adminDashboard/admDashboardHome/AdmDashboardHome.jsx
@@ -1,11 +1,14 @@
 import AdmEventSummery from "./AdmEventSummery";
 import AdmSummery from "./AdmSummery";
+import OrderSummery from "./OrderSummery";
 
 const AdmDashboardHome = () => {
     return (
         <div className="lg:ml-[300px] lg:pr-6">
             <AdmEventSummery />
             <AdmSummery />
+            <OrderSummery />
+            {/* Here will be the chart component */}
         </div>
     );
 };
