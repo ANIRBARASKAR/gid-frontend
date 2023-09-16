162c88430922c9bd50f981870c0db9cda2f42a19
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-09-16 12:49:38 +0600

    git add .

--- src/components/Pages/adminDashboard/admDashboardHome/AdmDashboardHome.jsx
+++ src/components/Pages/adminDashboard/admDashboardHome/AdmDashboardHome.jsx
@@ -1,9 +1,11 @@
 import AdmEventSummery from "./AdmEventSummery";
+import AdmSummery from "./AdmSummery";
 
 const AdmDashboardHome = () => {
     return (
-        <div>
+        <div className="lg:ml-[300px] lg:pr-6">
             <AdmEventSummery />
+            <AdmSummery />
         </div>
     );
 };
---
+++ src/components/Pages/adminDashboard/admDashboardHome/AdmSummery.jsx
@@ -0,0 +1,192 @@
+import AdmProgress from "./AdmProgress";
+import AmdLineProgress from "./AmdLineProgress";
+import SmallDeviceProgress from "./SmallDeviceProgress";
+
+
+const AdmSummery = () => {
+    return (
+        <div>
+            <div className='mt-5 lg:mt-16 mx-2 md:mx-0 border-2 border-gray-400 px-4 rounded-lg'>
+                <div className="overflow-x-auto w-full">
+                    <table className="w-[100%] text-xs md:text-base">
+                        <thead>
+                            <tr className='text-sm md:text-xl font-semibold text-[#001356] text-left'>
+                                <th className='py-4'>Events</th>
+                                <th className='py-4'>Registration</th>
+                                <th className='py-4 text-center'>Status</th>
+                                <th className='py-4 text-center'>Attendance</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr className="text-gray-500">
+                                <td className='py-4'>
+                                    <div>
+                                        <div className="flex items-center gap-2">
+                                            <img src="https://i.ibb.co/6gCPXTx/image-60.png" alt="" />
+                                            <div>
+                                                <p className="text-xs md:text-sm text-[#001356] font-medium">DEVOPS TRANNING & COURSE</p>
+                                                <p className="text-xs text-gray-400">#vf568dc96d</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td className='py-4 lg:px-4'>
+                                    <div className="hidden md:block">
+                                        <AmdLineProgress />
+                                    </div>
+                                    <div className="block md:hidden">
+                                        <SmallDeviceProgress />
+                                    </div>
+                                </td>
+                                <td className='lg:px-4'>
+                                    <div className="flex justify-center">
+                                        <button className="text-white bg-[#23AE00] px-4 py-1 rounded-lg">Published</button>
+                                    </div>
+                                </td>
+                                <td className='lg:px-4'>
+                                    <div className="flex justify-center">
+                                        <AdmProgress />
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                        <tbody>
+                            <tr className="text-gray-500">
+                                <td className='py-4'>
+                                    <div>
+                                        <div className="flex items-center gap-2">
+                                            <img src="https://i.ibb.co/6gCPXTx/image-60.png" alt="" />
+                                            <div>
+                                                <p className="text-xs md:text-sm text-[#001356] font-medium">DEVOPS TRANNING & COURSE</p>
+                                                <p className="text-xs text-gray-400">#vf568dc96d</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td className='py-4 lg:px-4'>
+                                    <div className="hidden md:block">
+                                        <AmdLineProgress />
+                                    </div>
+                                    <div className="block md:hidden">
+                                        <SmallDeviceProgress />
+                                    </div>
+                                </td>
+                                <td className='lg:px-4'>
+                                    <div className="flex justify-center">
+                                        <button className="text-white bg-[#23AE00] px-4 py-1 rounded-lg">Published</button>
+                                    </div>
+                                </td>
+                                <td className='lg:px-4'>
+                                    <div className="flex justify-center">
+                                        <AdmProgress />
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                        <tbody>
+                            <tr className="text-gray-500">
+                                <td className='py-4'>
+                                    <div>
+                                        <div className="flex items-center gap-2">
+                                            <img src="https://i.ibb.co/6gCPXTx/image-60.png" alt="" />
+                                            <div>
+                                                <p className="text-xs md:text-sm text-[#001356] font-medium">DEVOPS TRANNING & COURSE</p>
+                                                <p className="text-xs text-gray-400">#vf568dc96d</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td className='py-4 lg:px-4'>
+                                    <div className="hidden md:block">
+                                        <AmdLineProgress />
+                                    </div>
+                                    <div className="block md:hidden">
+                                        <SmallDeviceProgress />
+                                    </div>
+                                </td>
+                                <td className='lg:px-4'>
+                                    <div className="flex justify-center">
+                                        <button className="text-white bg-[#23AE00] px-4 py-1 rounded-lg">Published</button>
+                                    </div>
+                                </td>
+                                <td className='lg:px-4'>
+                                    <div className="flex justify-center">
+                                        <AdmProgress />
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                        <tbody>
+                            <tr className="text-gray-500">
+                                <td className='py-4'>
+                                    <div>
+                                        <div className="flex items-center gap-2">
+                                            <img src="https://i.ibb.co/6gCPXTx/image-60.png" alt="" />
+                                            <div>
+                                                <p className="text-xs md:text-sm text-[#001356] font-medium">DEVOPS TRANNING & COURSE</p>
+                                                <p className="text-xs text-gray-400">#vf568dc96d</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td className='py-4 lg:px-4'>
+                                    <div className="hidden md:block">
+                                        <AmdLineProgress />
+                                    </div>
+                                    <div className="block md:hidden">
+                                        <SmallDeviceProgress />
+                                    </div>
+                                </td>
+                                <td className='lg:px-4'>
+                                    <div className="flex justify-center">
+                                        <button className="text-white bg-[#23AE00] px-4 py-1 rounded-lg">Published</button>
+                                    </div>
+                                </td>
+                                <td className='lg:px-4'>
+                                    <div className="flex justify-center">
+                                        <AdmProgress />
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                        <tbody>
+                            <tr className="text-gray-500">
+                                <td className='py-4'>
+                                    <div>
+                                        <div className="flex items-center gap-2">
+                                            <img src="https://i.ibb.co/6gCPXTx/image-60.png" alt="" />
+                                            <div>
+                                                <p className="text-xs md:text-sm text-[#001356] font-medium">DEVOPS TRANNING & COURSE</p>
+                                                <p className="text-xs text-gray-400">#vf568dc96d</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td className='py-4 lg:px-4'>
+                                    <div className="hidden md:block">
+                                        <AmdLineProgress />
+                                    </div>
+                                    <div className="block md:hidden">
+                                        <SmallDeviceProgress />
+                                    </div>
+                                </td>
+                                <td className='lg:px-4'>
+                                    <div className="flex justify-center">
+                                        <button className="text-white bg-[#23AE00] px-4 py-1 rounded-lg">Published</button>
+                                    </div>
+                                </td>
+                                <td className='lg:px-4'>
+                                    <div className="flex justify-center">
+                                        <AdmProgress />
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AdmSummery;
