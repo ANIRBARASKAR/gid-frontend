407f6c8febd72312be14670208d00f1090bc8187
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-09-19 15:54:52 +0600

    circle progressbar

---
+++ src/components/Pages/adminDashboard/admEvent/ManageTable.jsx
@@ -0,0 +1,12 @@
+
+const ManageTable = () => {
+    return (
+        <>
+            <div className="lg:ml-[300px] lg:pr-6">
+
+            </div>
+        </>
+    );
+};
+
+export default ManageTable;
